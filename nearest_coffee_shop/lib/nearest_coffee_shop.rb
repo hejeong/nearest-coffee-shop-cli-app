ecbb8320bec632433ce51445ff962aefa271a979
Jonathan Hong <jonhong777@gmail.com>
2018-07-26 20:22:22 +0000

    Add compilable CLI

--- nearest_coffee_shop/lib/nearest_coffee_shop.rb
+++ nearest_coffee_shop/lib/nearest_coffee_shop.rb
@@ -1,5 +1,2 @@
-require "nearest_coffee_shop/version"
-
-module NearestCoffeeShop
-  # Your code goes here...
-end
+require_relative "./nearest_coffee_shop/version"
+require_relative "./nearest_coffee_shop/cli"
---
+++ nearest_coffee_shop/lib/nearest_coffee_shop/cli.rb
@@ -0,0 +1,8 @@
+class NearestCoffeeShop::CLI
+  def call
+    puts
+    puts
+    puts "Need coffee? Find the closest coffee shops around you!"
+    puts "To find your general location, we need your zipcode: "
+  end
+end
